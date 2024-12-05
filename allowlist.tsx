89ad586110537c5872179215130b97cb21e4b867
LE XUAN VU <5712xuanvu@gmail.com>
2024-12-06 03:15:55 +0700

    first commit

--- allowlist.tsx
+++ allowlist.tsx
@@ -3,18 +3,22 @@
 export const allowLists = new Map<string, Array<string>> ([
     ["WL",
     [
+      "6VnNArWU5cLq6pcrrCbQzAZRCjhtwjbLGaHY5vwzzx6j",
+      "EgvHRjSDhGffFesH4ahpozDCKspEcDU1HjcrorQBFiMA",
       "Tes1zkZkXhgTaMFqVgbgvMsVkRJpq4Y6g54SbDBeKVV",
       "GjwcWFQYzemBtpUoN5fMAP2FZviTtMRWCmrppGuTthJS",
       "AT8nPwujHAD14cLojTcB1qdBzA1VXnT6LVGuUd6Y73Cy"
     ]],
     ["OG",
     [
+      "EgvHRjSDhGffFesH4ahpozDCKspEcDU1HjcrorQBFiMA",
       "Ur1CbWSGsXCdedknRbJsEk7urwAvu1uddmQv51nAnXB",
       "GjwcWFQYzemBtpUoN5fMAP2FZviTtMRWCmrppGuTthJS",
       "AT8nPwujHAD14cLojTcB1qdBzA1VXnT6LVGuUd6Y73Cy",
     ]],
     ["default",
     [
+      "EgvHRjSDhGffFesH4ahpozDCKspEcDU1HjcrorQBFiMA",
       "Tes1zkZkXhgTaMFqVgbgvMsVkRJpq4Y6g54SbDBeKVV",
       "GjwcWFQYzemBtpUoN5fMAP2FZviTtMRWCmrppGuTthJS",
       "AT8nPwujHAD14cLojTcB1qdBzA1VXnT6LVGuUd6Y73Cy"
